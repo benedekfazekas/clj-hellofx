6e2353b28544d67cbe7b0731022b673c0452bd5e
Benedek Fazekas <benedek.fazekas@gmail.com>
2021-02-18 17:09:03 +0000

    add clojure version to label

--- src/thomasa/hellofx.clj
+++ src/thomasa/hellofx.clj
@@ -12,8 +12,11 @@
    :extends javafx.application.Application))
 
 (defn -start [_app ^Stage stage]
-  (let [label (Label. (str "Hello, JavaFX " (System/getProperty "javafx.version")
-                           ", running on Java " (System/getProperty "java.version") "."))
+  (let [{:keys [major minor incremental qualifier]} *clojure-version*
+        label (Label. (str "Hello, JavaFX " (System/getProperty "javafx.version")
+                           ", running on Java " (System/getProperty "java.version")
+                           (format " and on Clojure %d.%d.%d-%s" major minor incremental qualifier)
+                           "."))
         image-view (ImageView. (Image. (io/input-stream (io/resource "openduke.png"))))
         root (VBox. 30 (into-array Node [image-view label]))
         scene (Scene. root 640 480)]
