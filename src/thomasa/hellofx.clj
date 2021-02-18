c16bbdc964ea56eb418f413645c8fdd5775e6373
Benedek Fazekas <benedek.fazekas@gmail.com>
2021-02-18 16:43:55 +0000

    in pair with gluon HelloFX example
    - add image
    - add stylesheet and alignment

--- src/thomasa/hellofx.clj
+++ src/thomasa/hellofx.clj
@@ -1,7 +1,10 @@
 (ns thomasa.hellofx
+  (:require [clojure.java.io :as io])
   (:import [javafx.application Application]
+           [javafx.geometry Pos]
            [javafx.scene Scene Node]
            [javafx.scene.control Label]
+           [javafx.scene.image Image ImageView]
            [javafx.scene.layout VBox]
            [javafx.stage Stage])
   (:gen-class
@@ -11,8 +14,15 @@
 (defn -start [_app ^Stage stage]
   (let [label (Label. (str "Hello, JavaFX " (System/getProperty "javafx.version")
                            ", running on Java " (System/getProperty "java.version") "."))
-        root (VBox. 30 (into-array Node [label]))
+        image-view (ImageView. (Image. (io/input-stream (io/resource "openduke.png"))))
+        root (VBox. 30 (into-array Node [image-view label]))
         scene (Scene. root 640 480)]
+    (.setFitHeight image-view 200)
+    (.setPreserveRatio image-view true)
+
+    (.setAlignment root Pos/CENTER)
+
+    (.add (.getStylesheets scene) (.toExternalForm (io/resource "styles.css")))
     (.setScene stage scene)
     (.show stage)))
 
