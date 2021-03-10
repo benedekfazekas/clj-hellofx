aa6671a6763715f85ac8c73d308c8e795edd7b9c
Benedek Fazekas <benedek.fazekas@gmail.com>
2021-03-10 12:42:50 +0000

    try not to use `clojure.java.io`

--- src/thomasa/hellofx.clj
+++ src/thomasa/hellofx.clj
@@ -1,10 +1,10 @@
 (ns thomasa.hellofx
-  (:require [clojure.java.io :as io])
+  ;; (:require [clojure.java.io :as io])
   (:import [javafx.application Application]
            [javafx.geometry Pos]
            [javafx.scene Scene Node]
            [javafx.scene.control Label]
-           [javafx.scene.image Image ImageView]
+           ;[javafx.scene.image Image ImageView]
            [javafx.scene.layout VBox]
            [javafx.stage Stage])
   (:gen-class
@@ -17,15 +17,16 @@
                            ", running on Java " (System/getProperty "java.version")
                            (format " and on Clojure %d.%d.%d%s" major minor incremental (or (and qualifier (str "-" qualifier)) ""))
                            "."))
-        image-view (ImageView. (Image. (io/input-stream (io/resource "openduke.png"))))
-        root (VBox. 30 (into-array Node [image-view label]))
+        ;image-view (ImageView. (Image. (io/input-stream (io/resource "openduke.png"))))
+        root (VBox. 30 (into-array Node [;image-view
+                                         label]))
         scene (Scene. root 640 480)]
-    (.setFitHeight image-view 200)
-    (.setPreserveRatio image-view true)
+    ;(.setFitHeight image-view 200)
+    ;(.setPreserveRatio image-view true)
 
     (.setAlignment root Pos/CENTER)
 
-    (.add (.getStylesheets scene) (.toExternalForm (io/resource "styles.css")))
+    ;(.add (.getStylesheets scene) (.toExternalForm (io/resource "styles.css")))
     (.setScene stage scene)
     (.show stage)))
 
